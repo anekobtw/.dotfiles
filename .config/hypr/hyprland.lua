fe085016736cf14d08340598a0216568ec186149
anekobtw <elmars0608@gmail.com>
2026-09-24 01:16:06 -0400

    feat: removed waybar

--- .config/hypr/hyprland.lua
+++ .config/hypr/hyprland.lua
@@ -1,5 +1,4 @@
 hl.on("hyprland.start", function()
-	hl.exec_cmd("waybar")
 	hl.exec_cmd("hyprpaper")
 	hl.exec_cmd("dbus-update-activation-environment --systemd WAYLAND_DISPLAY XDG_CURRENT_DESKTOP")
 end)
